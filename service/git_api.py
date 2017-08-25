35bbe7b0fc359e30688e1b9526cecdfe23d424cc
Sonali Phatak <sphatak@prosper.com>
2017-08-25 13:20:41 -0700

    Updated git_api service

--- service/git_api.py
+++ service/git_api.py
@@ -12,6 +12,9 @@ class GetRequest(object):
         print "create_get_request", response.json()
         response_json = response.json()
         print response_json[0]["full_name"]
+        for i in range(0 ,25):
+            print response_json[i]["full_name"]
+
     """
     to get the no of commites
     """
@@ -23,7 +26,7 @@ class GetRequest(object):
 
 
 if __name__ == "__main__":
-    reponame= "svc-loans"
+    reponame = "platform-prospect"
     uname = "rajpalc"
     passw = "Welcome123@"
     GetRequest().create_get_request(uname,passw)
