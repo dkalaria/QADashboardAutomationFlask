00feeaeb9298418df40b1b86496c7b4f0cb06404
Dhruv Kalaria <dkalaria@prosper.com>
2017-08-25 14:38:30 -0700

    final changes to FE

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template, request,json, redirect, url_for
 import os
+import sys
 app = Flask(__name__)
  
 @app.route("/", methods=['GET'])
@@ -22,5 +23,10 @@ def dashboard():
 def logout():
 	return redirect(url_for('home'))
 
+# @app.route("/getJenkinsData", methods=['GET'])
+# def getJenkinsData():
+# 	return JenkinController.get_jenkin_job_test_results(JenkinController(),JenkinController().get_jenkin_user_auth(JenkinController(),'DKalaria','Jalebi@123'), base_url, "stg2-investor-platform-order-tests",)
+	
 if __name__ == "__main__":
+    sys.path.append('./service')
     app.run(debug=True)
